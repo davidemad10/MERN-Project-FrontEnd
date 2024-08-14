32762319cd4ca01f4dcb283c6ac7ef4766359ab2
Fady Mohsen <fadymohsen62@gmail.com>
2024-08-15 00:55:12 +0300

    Adding Home, bootstrap cdn and font-awesome cdn

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Homefooter from "./Homefooter";
+import Homeheader from "./Homeheader";
+// import Headeroffcanvas from "./HomeHeaderoffcanvas";
+
+function Home() {
+    return (
+        <>
+            <div className="container-fluid">
+                <Homeheader />
+                {/* <Headeroffcanvas /> */}
+                <Homefooter />
+            </div>
+        </>
+    )
+}
+
+export default Home;
