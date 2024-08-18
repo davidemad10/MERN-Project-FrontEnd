fdd9ee6221c0c862d83778c94450155473f54af9
David Emad <demad2527@gmail.com>
2024-08-19 02:04:57 +0300

    add book component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Home from "./components/Home";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BooksList from "./components/BookList";
 
 function App() {
   return (
