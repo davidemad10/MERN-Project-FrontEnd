07c1bad782f19fd02af4b1cc84c089f13c6646e6
Michael-Emad-Ramzy <michaelebeid9@gmail.com>
2024-08-19 17:10:38 +0300

    editing strucure

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import BooksList from "./components/BookList";
 function App() {
   return (
     <>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <BooksList/>
     </>
   );
 }
--- src/components/Homefooter.jsx
+++ src/components/Homefooter.jsx
@@ -2,15 +2,15 @@
 function Homefooter() {
     return (
         <>
-            <footer class="position-absolute bottom-0 start-50 translate-middle-x col-sm-10">
-                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Categories</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Books</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Authors</a></li>
-                    <li class="nav-item"><a href="about" class="nav-link px-2 text-body-secondary">About</a></li>
+            <footer className="position-absolute bottom-0 start-50 translate-middle-x col-sm-10">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Categories</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Books</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Authors</a></li>
+                    <li className="nav-item"><a href="about" className="nav-link px-2 text-body-secondary">About</a></li>
                 </ul>
-                <p class="text-center text-body-secondary">© 2024 DFM, Inc</p>
+                <p className="text-center text-body-secondary">© 2024 DFM, Inc</p>
             </footer>
         </>
     )
