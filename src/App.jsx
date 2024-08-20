22ac59b6a8dde50a7ff05c35d2b1b85c74aa15eb
Michael-Emad-Ramzy <michaelebeid9@gmail.com>
2024-08-20 23:53:10 +0300

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Home from "./components/Home";
 import BooksList from "./components/BookList";
 // import BookDetails from "./components/BookDetails";
-import "./styles/BookList.module.css";
+// import "./styles/BookList.module.css";
+import Home from "./components/Home";
+import AuthorList from "./components/AuthorList";
+import Login from "./components/Login";
+import Signup from "./components/signup/Signup";
 
 function App() {
   return (
@@ -14,7 +18,26 @@ function App() {
     //   </Routes>
     // </Router>
 
-    <BooksList></BooksList>
+    // <BooksList></BooksList>
+
+    // <AuthorList/>
+
+  //   <Router>
+  //   <Routes>
+  //     <Route path="/login" element={<Login />} />
+  //     <Route path="/dashboard" element={<h2>Dashboard</h2>} />
+  //     <Route path="/signup" element={<h2>Sign Up</h2>} />
+  //   </Routes>
+    // </Router>
+    
+
+    <Routes>
+    {user && <Route path="/" exact element={<Main />} />}
+    <Route path="/signup" exact element={<Signup />} />
+    {/* <Route path="/login" exact element={<Login />} /> */}
+    <Route path="/" element={<Navigate replace to="/login" />} />
+  </Routes>
+
   );
 }
 
