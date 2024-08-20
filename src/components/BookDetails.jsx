428aae1230597d73334e7edca4f715471dee0966
David Emad <demad2527@gmail.com>
2024-08-20 21:28:54 +0300

    book details page

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,50 +1,47 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/BookList.module.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-function BooksList() {
-  const [books, setBooks] = useState([]);
+function BooksDetails() {
+  const { id } = useParams();
+  const api_uri = "http://localhost:5000/books";
+  const [book, setBook] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
-    fetch("http://localhost:5000/books/id")
-      .then((response) => {
-        if (!response.ok) {
+    fetch(`${api_uri}/${id}`)
+      .then((res) => {
+        if (!res.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.json();
+        return res.json();
       })
       .then((data) => {
-        setBooks(data.data.books);
+        console.log("Fetched data:", data);
+        setBook(data);
         setLoading(false);
       })
-      .catch((error) => {
-        setError(error);
+      .catch((err) => {
+        setError(err.message);
         setLoading(false);
       });
-  }, []);
-
+  }, [id]);
   if (loading) {
     return <p>Loading...</p>;
   }
 
   if (error) {
-    return <p>Error: {error.message}</p>;
+    return <p>Error: {error}</p>;
   }
 
+  if (!book) {
+    return <p>No book found.</p>;
+  }
   return (
-    <div className={styles.book_list}>
-      {books.map((book) => (
-        <div className={styles.row} key={book._id}>
-          <div className={styles.card}>
-            <img src={book.image}></img>
-            <h2>{book.title}</h2>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <h1>product details {book.data.Book.title}</h1>
+    </>
   );
 }
 
-export default BooksList;
+export default BooksDetails;
--- src/components/Homeheader.jsx
+++ src/components/Homeheader.jsx
@@ -1,58 +1,85 @@
-import '../styles/Homeheader.css';
-import { useRef } from 'react';
+import "../styles/Homeheader.css";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 function Homeheader() {
+  const searchValue = useRef("");
 
-    const searchValue = useRef('');
+  function handleinput() {
+    console.log("Search value: ", searchValue.current.value);
+  }
 
-    function handleinput() {
-        console.log("Search value: ", searchValue.current.value)
-    };
-
-    return (
-        <>
-            <nav className="bg-light ">
-                {/* <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+  return (
+    <>
+      <nav className="bg-light ">
+        {/* <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button> */}
-                <div className="container-fluid d-flex flex-wrap    ">
-                    <ul className="nav me-auto">
-                        <li className="nav-item"><a href="/" className="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Categories</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Books</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Authors</a></li>
-                    </ul>
+        <div className="container-fluid d-flex flex-wrap    ">
+          <ul className="nav me-auto">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className="nav-link link-dark px-2 active"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/categories" className="nav-link link-dark px-2">
+                Categories
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/books" className="nav-link link-dark px-2">
+                Books
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/authors" className="nav-link link-dark px-2">
+                Authors
+              </Link>
+            </li>
+          </ul>
 
-                    <div className="me-auto my-auto border rounded ">
-                        <input type="text"
-                            style={{
-                                background: "white",
-                                border: 'none',
-                                borderRadius: '10px',
-                                paddingLeft: '1rem',
-                                // paddingTop: '0.1rem',
-                                // paddingBottom: '0.1rem',
-                                transition: '0.4s ease-in-out',
-                                fontWeight: '300'
-                            }}
-                            className="xx"
-                            placeholder="Type to search..."
-                            ref={searchValue}
-                        />
-                        <button className='headersearchicon'
-                            onClick={handleinput}
-                        >
-                            <i className="fa-solid fa-magnifying-glass"></i>
-                        </button>
-                    </div>
-                    <ul className="nav">
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Login</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Sign up</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+          <div className="me-auto my-auto border rounded ">
+            <input
+              type="text"
+              style={{
+                background: "white",
+                border: "none",
+                borderRadius: "10px",
+                paddingLeft: "1rem",
+                // paddingTop: '0.1rem',
+                // paddingBottom: '0.1rem',
+                transition: "0.4s ease-in-out",
+                fontWeight: "300",
+              }}
+              className="xx"
+              placeholder="Type to search..."
+              ref={searchValue}
+            />
+            <button className="headersearchicon" onClick={handleinput}>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link link-dark px-2">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link link-dark px-2">
+                Sign up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Homeheader;
