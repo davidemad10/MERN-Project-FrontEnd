4d6f25d427ff929a3a1bb82ba2fdbc27d019f2cc
David Emad <demad2527@gmail.com>
2024-08-24 00:19:14 +0300

    Edit author

--- src/components/Authordropmenu.jsx
+++ src/components/Authordropmenu.jsx
@@ -1,23 +1,15 @@
-
 function Authordropmenu() {
-    const authors = [
-        { label: 'Fady' },
-        { label: 'Mona' },
-        { label: 'SS' },
-        { label: 'AA77AA' },
-    ]
+  const authors = [{ label: "Fady" }, { label: "Mona" }, { label: "SS" }];
 
-    return (
-        <>
-            <select class="form-control mb-3 mt-1" id="exampleFormControlSelect1">
-                {
-                    authors.map((author, index) =>
-                        <option>{author.label}</option>
-                    )
-                }
-            </select>
-        </>
-    )
+  return (
+    <>
+      <select class="form-control mb-3 mt-1" id="exampleFormControlSelect1">
+        {authors.map((author, index) => (
+          <option>{author.label}</option>
+        ))}
+      </select>
+    </>
+  );
 }
 
 export default Authordropmenu;
